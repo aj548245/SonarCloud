aa95c7b00a35fd4a1af9b10dc01daebb67517686
aj548245 <38448421+aj548245@users.noreply.github.com>
2019-02-08 18:28:37 +0530

    Add files via upload

--- TsLintImport/src/app/app.component.ts
+++ TsLintImport/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  debugger;
+  a= undefined;
+  b=null;
   title = 'TsLintImport';
+  ngOnInit()
+  {
+    this.a===undefined ? 'test':'test1';
+    let a = "DoubleQuoteTest";
+    if(this.a==this.b)
+    {
+
+    }
+    else
+    {
+
+    }
+    let c=[1,2,3,4,5,6,7]
+    c.filter(a=>a=a)
+debugger;
+  }
 }
--- TsLintImport/src/test.ts
+++ TsLintImport/src/test.ts
@@ -8,7 +8,7 @@ import {
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
-
+var a : any;
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
